a6b9f7d9ecf379656e8473128cad75f37afd9024
John Nettles <chiefsquanto22@ksu.edu>
2020-09-10 13:09:57 -0500

    Test

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    public class IOrderItem
+    {
+        double Price { get; }
+        uint Calories { get; }
+        List<string> SpecialInstructions { get; }
+    }
+}
