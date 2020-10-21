93f515b5a4f6f2291fe2c77dbf81138b1bc0c29c
John Nettles <chiefsquanto22@ksu.edu>
2020-10-21 13:38:02 -0500

    Added Order.cs class

---
+++ Data/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    public class Order : INotifyPropertyChanged, INotifyCollectionChanged, ICollection
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private static int nextOrderNumber = 1;
+
+        public int Number { get; }
+
+        public double SalesTaxRate = .12;
+
+        public double Subtotal;
+
+        public double Tax;
+
+        public double Total;
+
+        public uint Calories;
+
+        public Order()
+        {
+            Number = nextOrderNumber;
+            nextOrderNumber++;
+        }
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
