79bc635e662d2ca37d881c646f7debf70e3ea69b
John Nettles <chiefsquanto22@ksu.edu>
2020-10-22 15:01:46 -0500

    Created Order.cs and Combo.cs. Combo is blank. Need to link event handling to GUI for Order

---
+++ Data/Combo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data
+{
+    public class Combo : IOrderItem
+    {
+        public double Price => throw new NotImplementedException();
+
+        public uint Calories => throw new NotImplementedException();
+
+        public List<string> SpecialInstructions => throw new NotImplementedException();
+    }
+}
