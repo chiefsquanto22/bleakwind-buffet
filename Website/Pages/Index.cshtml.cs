4f671672523599ac4f0340c60815d59355ff87bc
John Nettles <chiefsquanto22@ksu.edu>
2020-11-01 21:53:26 -0600

    Almost complete. Needs Combo listing and clean up main webpage

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading.Tasks;
+using BleakwindBuffet.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,16 +12,10 @@ namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public void OnGet()
         {
-
+            
         }
     }
 }
